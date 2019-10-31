2eee0de5d0e4fb1e5d27b4e4831feaf7ec9f7bd4
Max Melnychuk <m.melnychyk@skywell.software>
2019-10-31 15:10:40 +0200

    added configs

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   DB_NAME: 'postgres',
+  DB_TABLE: 'express_blog',
   DB_HOSTNAME: process.env.DB_HOSTNAME || 'localhost',
   DB_PORT: Number(process.env.DB_PORT) || 5432,
   DB_USERNAME: process.env.DB_USERNAME,
